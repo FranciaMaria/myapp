904527568660ae1bdbf36f3819458148a8c3932b
FranciaMaria <mariafrancia88@gmail.com>
2017-11-13 16:34:24 +0100

    layout, header, footer

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
